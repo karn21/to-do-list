a9a847b0ff11a4fb96b5553aa311085e5f5ee0b9
karn21 <karn212000@gmail.com>
2020-04-04 16:56:22 +0530

    added input component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,43 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 export class App extends Component {
+  state = {
+    items: [{ id: 1, task: "Wake up" }],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = () => {
+    console.log("handle Change");
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("handle Submit");
+  };
+  handleEdit = () => {
+    console.log("handle Edit");
+  };
+  handleDelete = () => {
+    console.log("handle Delete");
+  };
+
   render() {
     return (
-      <div>
-        <TodoInput></TodoInput>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-10 mx-auto mt-3">
+            <h3 className="text-capitalize text-center">todo Input</h3>
+          </div>
+          <div className="col-10 col-md-8 offset-md-2 offset-1">
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            ></TodoInput>
+          </div>
+        </div>
+
         <TodoList></TodoList>
       </div>
     );
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,32 @@ import React, { Component } from "react";
 
 export class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit } = this.props;
     return (
-      <div>
-        <h1>Todo Input</h1>
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary">
+                <i className="fas fa-book text-white"></i>
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="Add Todo item"
+              name="item"
+              onChange={handleChange}
+              value={item}
+            />
+          </div>
+          <button
+            className="btn-primary btn btn-block mt-4 text-uppercase"
+            type="btn"
+          >
+            Add Item
+          </button>
+        </form>
       </div>
     );
   }
